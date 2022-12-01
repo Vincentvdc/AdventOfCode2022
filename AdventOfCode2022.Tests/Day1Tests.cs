9853a64565bc6d9e726018a5c965c353b56c3d35
Vincent van de Crommert <vincent.vandecrommert@hotmail.com>
2022-12-01 16:59:48 +0100

    Add project files.

---
+++ AdventOfCode2022.Tests/Day1Tests.cs
@@ -0,0 +1,27 @@
+using Microsoft.VisualStudio.TestPlatform.Utilities;
+using Xunit.Abstractions;
+
+namespace AdventOfCode2022.Tests
+{
+    public class Day1Tests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public Day1Tests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void Challenge1()
+        {
+            _output.WriteLine(Day1.SolveChallenge1().ToString());
+        }
+
+        [Fact]
+        public void Challenge2()
+        {
+            _output.WriteLine(Day1.SolveChallenge2().ToString());
+        }
+    }
+}
---
+++ AdventOfCode2022/Solutions/Day1.cs
@@ -0,0 +1,27 @@
+﻿namespace AdventOfCode2022.Solutions;
+
+/// <summary>
+/// Solution to the first day of Advent of code 2022
+/// </summary>
+public static class Day1
+{
+    public static int SolveChallenge1()
+    {
+        return ParseInput().First();
+    }
+
+    public static int SolveChallenge2()
+    {
+        return ParseInput().Sum();
+    }
+
+    private static IEnumerable<int> ParseInput()
+    {
+        return Inputs.Day1.Split("\r\n\r\n") // Splits per elf
+                          .Select(elf => elf.Split("\r\n") // Splits individual calories for every elf
+                          .Select(calories => Convert.ToInt32(calories)))
+                          .Select(calories => calories.Sum())
+                          .OrderByDescending(calories => calories)
+                          .Take(3); // We only care about the top 3 records for these challenges
+    }
+}
