077e18c4f9c39b6aca8ce1b818be33179bef69a8
Vincent van de Crommert <vincent.vandecrommert@hotmail.com>
2022-12-05 20:48:19 +0100

    Day 4

---
+++ AdventOfCode2022.Tests/Solutions/Day4Tests.cs
@@ -0,0 +1,34 @@
+﻿namespace AdventOfCode2022.Tests.Solutions;
+public class Day4Tests
+{
+    private readonly ITestOutputHelper _output;
+
+    public Day4Tests(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
+    [Fact]
+    public void Challenge1()
+    {
+        string input = "2-4,6-8\r\n2-3,4-5\r\n5-7,7-9\r\n2-8,3-7\r\n6-6,4-6\r\n2-6,4-8";
+        Day4 day = new(input);
+
+        Assert.Equal(2, day.Challenge1());
+
+        day = new(Inputs.Day4);
+        _output.WriteLine(day.Challenge1().ToString());
+    }
+
+    [Fact]
+    public void Challenge2()
+    {
+        string input = "2-4,6-8\r\n2-3,4-5\r\n5-7,7-9\r\n2-8,3-7\r\n6-6,4-6\r\n2-6,4-8";
+        Day4 day = new(input);
+
+        Assert.Equal(4, day.Challenge2());
+
+        day = new(Inputs.Day4);
+        _output.WriteLine(day.Challenge2().ToString());
+    }
+}
---
+++ AdventOfCode2022/Solutions/Day4.cs
@@ -0,0 +1,49 @@
+﻿namespace AdventOfCode2022.Solutions;
+public class Day4 : DayBase<int>
+{
+    public Day4(string input) : base(input)
+    {
+    }
+
+    public override int Challenge1()
+    {
+        var pairs = ParseInputs();
+        return pairs.Count(CheckIfAssignmentsFullyOverlap);
+    }
+
+    public override int Challenge2()
+    {
+        var pairs = ParseInputs();
+        return pairs.Count(CheckIfAssignmentsOverlap);
+    }
+    public static bool CheckIfAssignmentsFullyOverlap((IEnumerable<int> assignment1, IEnumerable<int> assignment2) pair)
+    {
+        int intersecting = pair.assignment1.Intersect(pair.assignment2).Count();
+        return intersecting == pair.assignment1.Count() || intersecting == pair.assignment2.Count();
+    }
+
+    public static bool CheckIfAssignmentsOverlap((IEnumerable<int> assignment1, IEnumerable<int> assignment2) pair)
+    {
+        int intersecting = pair.assignment1.Intersect(pair.assignment2).Count();
+        return intersecting > 0;
+    }
+
+    private IEnumerable<(IEnumerable<int> assignment1, IEnumerable<int> assignment2)> ParseInputs()
+    {
+        IEnumerable<(string assignment1, string assignment2)> parsedLines = _input.Split("\r\n").Select(pair => pair.Split(',')).Select(pair => (pair[0], pair[1]));
+
+        return parsedLines.Select(pair => (ExtractRange(pair.assignment1), ExtractRange(pair.assignment2)));
+
+    }
+
+    private static IEnumerable<int> ExtractRange(string assignment)
+    {
+        string[] range = assignment.Split('-');
+        int start = Convert.ToInt32(range[0]);
+        int end = Convert.ToInt32(range[1]);
+        int count = end - start + 1;
+        return Enumerable.Range(start, count);
+
+    }
+
+}
